0106684f4d8cd18550ed502a0ab4c7f2bf375d3e
Alexander Chernykh <dev@whitered932.ru>
2024-05-29 16:47:26 +0500

    разрешил сотруднику получать столики

--- BookService.Admin.Startup/Controllers/Admin/TablesController.cs
+++ BookService.Admin.Startup/Controllers/Admin/TablesController.cs
@@ -1,25 +1,31 @@
 using BookService.Admin.Startup.Features.Tables;
 using BookService.Admin.Startup.Features.Tables.Models;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookService.Admin.Startup.Controllers.Admin;
 
 public class TablesController(IMediator mediator) : AdminBaseController(mediator)
 {
+    [Authorize(Roles = "Admin,Employee")]
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<TableDto>>> GetMany(GetTablesQuery query) =>
         await Execute(query);
 
+    [Authorize(Roles = "Admin,Employee")]
     [HttpGet("{id:long}")]
     public async Task<ActionResult<TableDto>> Get(GetTableQuery query) => await Execute(query);
 
+    [Authorize(Roles = "Admin")]
     [HttpPost]
     public async Task<IActionResult> Create(CreateTableCommand command) => await Execute(command);
 
+    [Authorize(Roles = "Admin")]
     [HttpDelete("{id:long}")]
     public async Task<IActionResult> Delete(DeleteTableCommand command) => await Execute(command);
 
+    [Authorize(Roles = "Admin")]
     [HttpPut("{id:long}")]
     public async Task<IActionResult> Update(UpdateTableCommand command) => await Execute(command);
 }
